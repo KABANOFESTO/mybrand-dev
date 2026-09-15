fd38e37d5e479877c2f2a2838616919cb1f61c60
KABANOFESTO <kabanofesto.developer@gmail.com>
2026-09-15 14:58:13 +0200

    fix: add dashboard content spacing below navbars

--- fe/src/pages/vistor/layout.jsx
+++ fe/src/pages/vistor/layout.jsx
@@ -51,7 +51,7 @@ const VisitorLayout = () => {
 					sidebarOpen={sidebarOpen}
 					onMenuToggle={() => setSidebarOpen((current) => !current)}
 				/>
-				<main className="flex-1 overflow-y-auto px-4 pb-6 pt-0 text-[#e7e9f2] sm:px-6 lg:px-8">
+				<main className="flex-1 overflow-y-auto px-4 pb-6 pt-3 text-[#e7e9f2] sm:px-6 sm:pt-4 lg:px-8 lg:pt-5">
 					<Outlet context={{ activeItem, closeSidebar: () => setSidebarOpen(false) }} />
 				</main>
 			</div>
